b34646f3aa7f08d3a70accce247195289def9569
chernyak88 <i_chernyak@inbox.ru>
2019-03-26 00:08:45 +0300

    Исправил замечания

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,14 +1,23 @@
-// const anchors = document.querySelectorAll('a[href*="#"]')
+const anchors = document.querySelectorAll('a[href*="#"]')
 
-// for (let anchor of anchors) {
-//   anchor.addEventListener('click', function (e) {
-//     e.preventDefault()
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
     
-//     const blockID = anchor.getAttribute('href')
+    const blockID = anchor.getAttribute('href')
     
-//     document.querySelector('' + blockID).scrollIntoView({
-//       behavior: 'smooth',
-//       block: 'start'
-//     })
-//   })
-// }
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+const scrollBtn = document.querySelector(".hero__bottom-link");
+const reqBlock = document.querySelector("section.about");
+
+scrollBtn.addEventListener("click", e => {
+e.preventDefault();
+
+reqBlock.scrollIntoView({ block: "center", behavior: "smooth" });
+});
