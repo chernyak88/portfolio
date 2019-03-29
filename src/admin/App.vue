821e1fa6a3df66c82c01668738551b680dff96c6
chernyak88 <i_chernyak@inbox.ru>
2019-03-29 14:25:02 +0300

    Сверстал header админки

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,38 @@
 <template lang="pug">
-  h1 Welcome to the Vue App
+.wrapper.admin-wrapper
+  .maincontent
+    .root-wrapper-container
+      .root-container
+        .header-container
+          header.header
+           .container
+            .header__info
+              .admin-user
+                .admin-user__avatar
+                  img.admin-user__avatar-pic(src="../images/content/user.jpg")
+                .admin-user__name Игорь Черняк
+              .header__title Панель администрирования
+            a.exit-btn Выйти 
+        .tabs-container
+          .tabs__content
+            .admin-header__menu
+              nav.nav
+                ul.nav__list
+                  - var menu = [{name: 'Обо мне'}, {name: 'Работы'}, {name: 'Отзывы'}];
+                  each item in menu
+                    li.nav__item
+                      a.nav__link #{item.name}
+      .tooltips-container
 </template>
+
+<style lang="postcss">
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800');
+@import "normalize.css";
+@import "../styles/mixins.pcss";
+@import "../styles/layout/base.pcss";
+
+@import "../styles/blocks/about.pcss";
+@import "../styles/blocks/nav.pcss";
+@import "../styles/admin/admin-base.pcss";
+@import "../styles/admin/admin-header.pcss";
+</style>
