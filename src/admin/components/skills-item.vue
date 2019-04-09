1e0dd57b046e377fd18a6aaad8d2436c32bd6cf0
chernyak88 <i_chernyak@inbox.ru>
2019-04-09 16:39:57 +0300

    Редактирование скилла

--- src/admin/components/skills-item.vue
+++ src/admin/components/skills-item.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  tr.about-block-row
+  tr(v-if="editmode === false").about-block-row
     td.about-block-cell {{skill.title}}
     td.about-block-cell {{skill.percent}}
     td.about-block-cell %
@@ -7,6 +7,16 @@
       button(type="button").admin-btn.edit__btn
     td.about-block-cell
       button(type="button" @click="removeExistedSkill").admin-btn.remove__btn
+  tr(v-else).about-block-row
+    td.about-block-cell
+      input(type="text" v-model="editedSkill.title") 
+    td.about-block-cell
+      input(type="text" v-model="editedSkill.percent") 
+    td.about-block-cell %
+    td.about-block-cell
+      button.admin-btn.apply__btn
+    td.about-block-cell
+      button.admin-btn.close__btn
 </template>
 
 <script>
@@ -14,6 +24,12 @@ import { mapActions } from "vuex";
 export default {
   props: {
     skill: Object
+  },
+  data() {
+    return {
+      editmode: false,
+      editedSkill: {}
+    }
   },
   methods: {
     ...mapActions('skills', ['removeSkill']),
